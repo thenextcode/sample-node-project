535a83d4b4491b153d957e48a32548b7c4c37c6b
Priyesh Gaikwad <priyesh.gaikwad@gmail.com>
2025-01-17 16:23:52 +0530

    Update server.test.js

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
-const app = require('../server');
+const { app, server } = require('../server'); // Import both the app and server
 
 describe('GET /', () => {
+  afterAll(() => {
+    // Close the server after all tests
+    server.close();
+  });
+
   it('should return "Hello, World!"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
